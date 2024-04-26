3152890621543f7dcbd8a85a3308e750e418801f
aysegullkadiroglu <aysegullkadiroglu@gmail.com>
2024-04-26 09:37:42 +0300

    Add OOP

---
+++ src/OOP/ProductMain.java
@@ -0,0 +1,69 @@
+package OOP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+
+        ProductManager productManager = new ProductManager();
+
+        List<Product> products = productManager.getAll();
+       /* for (Product product1:products) {
+            System.out.println(product1.getName());
+        }
+
+        System.out.println("-----------------------------------------------");
+
+
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Çamaşır Makinesi");
+
+        System.out.println(product.getName() + " " + product.getUnitPrice());
+
+        System.out.println("-----------------------------------------------");
+
+        productManager.addBasketProduct(product);
+        productManager.add(product);
+
+        for (Product product1:products) {
+            System.out.println(product1.getName());
+        }
+
+        System.out.println("-----------------------------------");
+
+        Product responseProduct = productManager.getById(2);
+        System.out.println(responseProduct.getId() + " " + responseProduct.getName() + " "
+                + responseProduct.getUnitPrice());
+
+        System.out.println("-----------------------------------");
+
+        Product responseProduct1 = productManager.getByName("Laptop");
+        System.out.println(responseProduct1.getId() + " " + responseProduct1.getName() + " "
+                + responseProduct1.getUnitPrice());
+
+
+        System.out.println("---------------------------------");
+        Product updatedProduct = new Product();
+        updatedProduct.setId(1);
+        updatedProduct.setName("Bulaşık Makinesi");
+        updatedProduct.setUnitPrice(11000);
+        productManager.update(updatedProduct);
+
+        for (Product product1:products) {
+            System.out.println(product1.getName() + " " + product1.getUnitPrice());
+        }*/
+
+        System.out.println("---------------------------------");
+        productManager.delete(3);
+        for (Product product1:products) {
+            System.out.println(product1.getName() + " " + product1.getUnitPrice());
+        }
+
+    }
+
+
+
+}
---
+++ src/OOP/ProductManager.java
@@ -0,0 +1,74 @@
+package OOP;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ProductManager {
+    private final List<Product> products;
+
+    public ProductManager(){
+        products = new ArrayList<Product>();
+        products.add(new Product(1,"Laptop",25000,2));
+        products.add(new Product(2,"Tablet",12000,5));
+        products.add(new Product(3,"Çamaşır Makinesi",11000,5));
+    }
+
+    public List<Product> getAll() {
+        return products;
+    }
+
+    public void addBasketProduct(Product product) {
+        System.out.println("Başarıyla sepete eklendi " + product.getName());
+    }
+
+    public void add(Product product) {
+        products.add(product);
+    }
+
+
+    public Product getById(int id) {
+        for(Product product:products) {
+            if(product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product getByName(String name) {
+        for (Product product:products) {
+            if(product.getName() == name) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void update(Product product) {
+        for (Product updateProduct:products) {
+            if (updateProduct.getId() == product.getId()) {
+                updateProduct.setName(product.getName());
+                updateProduct.setUnitPrice(product.getUnitPrice());
+            }
+        }
+    }
+
+
+    public void delete(int id) {
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getId() == id){
+                iterator.remove();
+            }
+        }
+        /*for(Product product:products){
+            if (product.getId()==id){
+                products.remove(id-1);
+            }
+        }*/
+    }
+}
+
+
